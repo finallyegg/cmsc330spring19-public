7d15f77a6742f045f4be879ad21c959249795b7d
Carl <thelastdobe@gmail.com>
2019-08-14 05:56:14 +0800

    8/13

--- project2a/src/basics.ml
+++ project2a/src/basics.ml
@@ -37,29 +37,45 @@ let rec is_even_sum lst = match lst with
     | a :: b -> if ((a mod 2 = 0) && is_even_sum b) || (a mod 2 = 1) && not (is_even_sum b) then true else false  
 
 let rec count_occ lst target = match lst with
+        | [] -> 0
+        | a :: b -> if a = target then 1 + count_occ b target else count_occ b target
+let rec count_occ1 lst target = match lst with
     | [] -> false 
-    | a :: b -> if a = target then true else count_occ b target
+    | a :: b -> if a = target then true else count_occ1 b target
 
 let rec dup_list lst = match lst with
     | [] -> false
-    | a::b -> if (count_occ b a) then true else dup_list b
+    | a::b -> if (count_occ1 b a) then true else dup_list b
 
 (****************)
 (* Part 3: Sets *)
 (****************)
 
-let rec elem x a = failwith "unimplemented"
+let rec elem x a = count_occ1 a x
 
-let rec insert x a = failwith "unimplemented"
+let rec insert x a = match a with
+        | [] -> [x]
+        | _ -> if (count_occ1 a x) then a else x::a 
 
-let rec subset a b = failwith "unimplemented"
+let rec subset a b = match a with
+        | []-> true
+        | x::y -> if (count_occ1 b x) then subset y b else false 
 
-let rec eq a b = failwith "unimplemented"
+let rec eq a b = ((subset a b )&&( subset b a))
 
-let rec remove x a = failwith "unimplemented"
+let rec remove x a = match a with
+        | [] -> []
+        | j::k -> if j = x then k else j::(remove x k)
 
-let rec union a b = failwith "unimplemented"
+let rec union a b = match a with
+        | [] -> b
+        | x::y -> union y (insert x b) 
+let rec diff_help a b = match a with
+        | [] -> []
+        | x::y -> if (count_occ1 b x) then diff_help y b else x:: diff_help y b
 
-let rec diff a b = failwith "unimplemented"
+let rec diff a b = union(diff_help b a) (diff_help a b)
 
-let rec cat x a = failwith "unimplemented"
+let rec cat x a = match a with 
+        | [] -> []
+        | j::k -> (x,j)::cat x k
